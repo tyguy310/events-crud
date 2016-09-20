7fb247bf552ddfb9a7c6a7aa445954c1cfc7ad6e
Tyler Maier <tyler_maier@yahoo.com>
2016-09-20 12:13:42 -0600

    sets up routes

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -6,9 +6,15 @@
 
     // *** routes *** //
     const routes = require('../routes/index');
+    const events = require('../routes/events');
+    const attendees = require('../routes/attendees');
+    const venues = require('../routes/venues');
 
     // *** register routes *** //
     app.use('/', routes);
+    app.use('/events', events);
+    app.use('/attendees', attendees);
+    app.use('/venues', venues);
 
   };
 
--- src/server/routes/events.js
+++ src/server/routes/events.js
@@ -5,12 +5,12 @@ const indexController = require('../controllers/index');
 
 router.get('/', function (req, res, next) {
   const renderObject = {};
-  renderObject.title = 'Event App';
+  renderObject.title = 'Welcome to Express!';
   indexController.sum(1, 2, (error, results) => {
     if (error) return next(error);
     if (results) {
       renderObject.sum = results;
-      res.render('index', renderObject);
+      res.render('events', renderObject);
     }
   });
 });
